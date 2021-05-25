b7fe413d6cdf29b30c298ef60d20eb4ebb9facb6
Amarok909 <Amarok909@Hotmail.com>
2021-05-25 23:47:30 +1000

    Adds 'boipussy' as an anus descriptor

--- src/com/lilithsthrone/game/character/body/abstractTypes/AbstractAnusType.java
+++ src/com/lilithsthrone/game/character/body/abstractTypes/AbstractAnusType.java
@@ -161,7 +161,7 @@ public abstract class AbstractAnusType implements BodyPartTypeInterface {
 	@Override
 	public String getNameSingular(GameCharacter gc) {
 		if(Main.game.isSillyModeEnabled()) {
-			return UtilText.returnStringAtRandom("bussy", "bussy", "bunghole", "pooper");
+			return UtilText.returnStringAtRandom("bussy", "bussy", "boipussy", "bunghole", "pooper");
 		}
 		if(names==null || names.isEmpty()) {
 			return UtilText.returnStringAtRandom("asshole", "back door", "rear entrance");
@@ -172,7 +172,7 @@ public abstract class AbstractAnusType implements BodyPartTypeInterface {
 	@Override
 	public String getNamePlural(GameCharacter gc) {
 		if(Main.game.isSillyModeEnabled()) {
-			return UtilText.returnStringAtRandom("bussies", "bussies", "bungholes", "poopers");
+			return UtilText.returnStringAtRandom("bussies", "bussies", "boipussies", "bungholes", "poopers");
 		}
 		if(namesPlural==null || namesPlural.isEmpty()) {
 			return UtilText.returnStringAtRandom("assholes", "back doors", "rear entrances");
